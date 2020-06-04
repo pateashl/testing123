dc1fb2082febb8dc9a3e304f838d53077272fba7
pateashl <pateashl@sheridancollege.ca>
2020-06-03 23:11:03 -0400

    Practice Test2

--- src/javaapplication18/JavaApplication18.java
+++ src/javaapplication18/JavaApplication18.java
@@ -16,6 +16,8 @@ public class JavaApplication18 {
      */
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello2");       
+                
     }
     
 }
